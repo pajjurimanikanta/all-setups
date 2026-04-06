d91f525d089b4038b9373be191c0ddce6d7d67d9
pajjurimanikanta <85977827+pajjurimanikanta@users.noreply.github.com>
2026-04-06 21:53:40 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -3,9 +3,9 @@
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
+
 # Add required dependencies for the jenkins package
-yum install java-17-amazon-corretto jenkins -y
+yum install java-21-amazon-corretto jenkins -y
 sudo yum install jenkins -y
-sudo systemctl restart jenkins
-sudo systemctl daemon-reload
+ systemctl start jenkins.service
+ systemctl status jenkins.service
